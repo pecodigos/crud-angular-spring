1d012647ec7950253e0cc5b42c32bbb4c2f5b84e
pecodigos <pedrenriquecunha@gmail.com>
2024-10-14 23:53:55 -0300

    First spring tests

--- crud_spring/src/main/java/com/pecodigos/crud_spring/controller/GameController.java
+++ crud_spring/src/main/java/com/pecodigos/crud_spring/controller/GameController.java
@@ -5,6 +5,7 @@ import com.pecodigos.crud_spring.repository.GameRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,7 +18,7 @@ public class GameController {
     private final GameRepository gameRepository;
 
     @GetMapping
-    public List<Game> list() {
+    public @ResponseBody List<Game> list() {
         return gameRepository.findAll();
     }
 }
--- crud_spring/src/main/java/com/pecodigos/crud_spring/model/Game.java
+++ crud_spring/src/main/java/com/pecodigos/crud_spring/model/Game.java
@@ -1,5 +1,6 @@
 package com.pecodigos.crud_spring.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -9,6 +10,7 @@ public class Game {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty("_id")
     private Long id;
 
     @Column(length = 200, nullable = false)
