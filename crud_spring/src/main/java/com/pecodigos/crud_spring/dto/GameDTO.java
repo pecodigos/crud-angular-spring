ee4825279632ae37d216ba5ba5478a0b6c04bd10
pecodigos <pedrenriquecunha@gmail.com>
2024-10-17 07:13:27 -0300

    GameDTO implementation

--- crud_spring/src/main/java/com/pecodigos/crud_spring/dto/GameDTO.java
+++ crud_spring/src/main/java/com/pecodigos/crud_spring/dto/GameDTO.java
@@ -1,4 +1,12 @@
 package com.pecodigos.crud_spring.dto;
 
-public class GameDTO {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
+
+public record GameDTO(Long _id,
+                      @NotNull @NotBlank @Length(min = 2, max = 100) String name,
+                      @NotNull @Length(max = 20) @Pattern(regexp = "Turn-based RPG|FPS") String genre,
+                      @NotNull @Length(max = 20) @Pattern(regexp = "Playstation 1|PC") String platform) {
 }
